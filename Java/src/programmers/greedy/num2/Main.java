a658531c405b267e911b36452c69fae38ff73242
ydj515 <ydj515@hanmail.net>
2020-03-26 14:46:40 +0900

    programmers greedy num2 commit

--- Java/src/programmers/greedy/num2/Main.java
+++ Java/src/programmers/greedy/num2/Main.java
@@ -1,35 +1,83 @@
 import java.util.*;
 class Solution {
-	public String solution(String number, int k) {
-		int idx = 0;
-		char max;
-		StringBuilder answer = new StringBuilder(); // 처음에 String에다가 max += 해줬는데 시간초과 나서 builder썻음
+    public int solution(String name) {
 
-		// 시작을 0으로 주는 파렴치한 짓을 하네;
-		// 예외 처리함
-		if (number.charAt(0) == '0') {
-			return "0";
-		}
+        int answer = 0;
+        int now = 0;
 
-		for (int i = 0; i < number.length() - k; i++) {
+        String tempString = "";
+        String name2 = name.toString();
 
-			max = '0';
+        // System.out.println(name2);
+        for (int i = 0; i < name.length(); i++) {
+            tempString += "A";
+        }
 
-			for (int j = idx; j <= k + i; j++) {
-				
-				System.out.println("number.charat(" + j + ") : " + number.charAt(j) + ", max : " + max);
-				
-				if (max < number.charAt(j)) {
-					max = number.charAt(j);
-					idx = j + 1;
-				}
-			}
-			answer.append(max);
-			System.out.println(answer);
-		}
-		System.out.println("=================");
-		System.out.println(answer.toString());
+        // N(13)을 기준으로 좌우 판단
+        // 알파벳 몇 칸 움직여야하는지 판단
+        for (int i = 0; i < name.length(); i++) {
 
-		return answer.toString();
-	}
+            if (name.charAt(i) - 'A' > 13) {
+                answer += 'Z' - name.charAt(i) + 1;
+                // System.out.println('Z' - name.charAt(i) + 1);
+            } else {
+                answer += name.charAt(i) - 'A';
+                // System.out.println(name.charAt(i) - 'A');
+            }
+        }
+
+        System.out.println("answer : " + answer);
+
+        // 맨 왼쪽에서 오른쪽으로 쭉 가는거
+        int count = 0;
+
+        for (int i = 1; i < name.length(); i++) {
+            if (tempString.equals(name)) {
+                break;
+            }
+            count++;
+            if (name.charAt(i) != 'A') {
+
+                StringBuilder myName = new StringBuilder(name);
+                myName.setCharAt(i, 'A');
+                name = myName.toString();
+                System.out.println(name);
+            }
+            System.out.println("count : " + count);
+        }
+        System.out.println(count);
+
+
+        // 맨 오른쪽에서 왼쪽으로 쭉 가는거
+        int count2 = 0;
+
+        if (name2.charAt(0) != 'A') {
+            StringBuilder myName = new StringBuilder(name2);
+            myName.setCharAt(0, 'A');
+            name2 = myName.toString();
+        }
+
+        System.out.println("name2 : " + name2);
+
+        for (int i = name.length() - 1; i >= 1; i--) {
+            if (tempString.equals(name2)) {
+                break;
+            }
+            count2++;
+            if (name2.charAt(i) != 'A') {
+
+                StringBuilder myName = new StringBuilder(name2);
+                myName.setCharAt(i, 'A');
+                name2 = myName.toString();
+                System.out.println(name2);
+            }
+        }
+        System.out.println(count2);
+
+        answer += Math.min(count, count2);
+
+        System.out.println("answer : " + answer);
+
+        return answer;
+    }
 }
