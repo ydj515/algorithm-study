41be9706ad75d930c0aa020517d089d4029dcfea
ydj515 <ydj515@hanmail.net>
2020-03-27 18:09:18 +0900

    programmers stack & queue num6 update commit

--- Java/src/programmers/stackqueue/num6/Main.java
+++ Java/src/programmers/stackqueue/num6/Main.java
@@ -1,8 +1,10 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
 
         // 문제 이해 난이도가 더 어려워;;
         // 주식 한개의 가격이다. 여러개가 아님.
+        // 바로 뒤에거 까지 비교
 
         int[] answer = new int[prices.length];
         int cnt = 0;
@@ -12,6 +14,7 @@ class Solution {
             cnt = 0;
 
             for (int j = i + 1; j < prices.length; j++) {
+                // System.out.println("price i : " + prices[i] + ", price j : " + prices[j]);
                 if (prices[i] <= prices[j]) {
                     cnt++;
                 } else {
@@ -19,13 +22,13 @@ class Solution {
                     break;
                 }
             }
+            // System.out.println("========" + "cnt : " + cnt);
             answer[i] = cnt;
         }
 
         // 마지막은 무조건 0 이자나.
         answer[prices.length - 1] = 0;
-
-
+        
         return answer;
     }
 }
